b8bb9eb61af2f93371dd98059a5115106fa2a41c
gildo <Gildo-SN@hotmail.com>
2021-04-03 00:01:26 -0300

    added api keys to env example and functionality that reads the image that you replied and replies back to you with the text from the image

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 require("dotenv").config()
+const vision = require('@google-cloud/vision');
+const visionClient = new vision.ImageAnnotatorClient({
+    credentials: JSON.parse(process.env.VISION_PRIVATE_KEY_AND_CLIENT_EMAIL_JSON),
+    projectId: process.env.PROJECT_ID,
+});
+
+//detects all text from image and reply it to who write /lepramim
+async function detectTextFromImage(imagePath, msg) {
+    const [result] = await visionClient.textDetection(imagePath);
+    const text = result.textAnnotations;
+    msg.reply('\n' + text[0].description)
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -11,6 +23,21 @@ client.on("message", msg => {
 
     if (msg.content === "ping") {
         msg.reply("Pong!")
+    }
+
+    if (msg.content === "/lepramim") {
+        msg.channel.messages.fetch(msg.reference.messageID)
+            .then(
+                message => {
+                    attachments = (message.attachments).array();
+                    if(attachments.length > 0 && typeof attachments !== 'undefined') {
+                        attachments.forEach(function(attachment) {
+                            detectTextFromImage(attachment.url, msg);
+                        })
+                    }
+                }
+            )
+            .catch(console.error);
     }
 
     var Attachment = (msg.attachments).array();
@@ -30,12 +57,12 @@ client.on("message", msg => {
 client.on("voiceStateUpdate", (oldState,newState) => {
     const voiceStatus = newState.selfDeaf;
     const mem = newState.member;
-    if (voiceStatus) {
+    if (voiceStatus && mem.voice.channelID != process.env.AFK_CHANNEL_ID) {
         client.channels.fetch(process.env.AFK_CHANNEL_ID)
             .then(
                 channel => mem.voice.setChannel(channel)
             );
     }
-  })
+})
 
 client.login(process.env.BOT_TOKEN)
